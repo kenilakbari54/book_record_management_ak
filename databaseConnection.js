a88cc4a7163f1b1766a4e8cb5cade2dde21d3fdf
Kenil Akbari <89933076+kenilakbari54@users.noreply.github.com>
2022-12-23 19:35:20 +0530

    Add files via upload

---
+++ databaseConnection.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+function DbConnection() {
+    const DB_URL = process.env.MONGO_URI;
+
+    mongoose.connect(DB_URL, {
+        //when throw error
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+    });
+
+    const db = mongoose.connection;
+
+    db.on("error", console.error.bind("Connection error"));
+    db.once("open", function () {
+        console.log("DB Connected !!");
+    });
+}
+
+// CRUD => Create, Read, Update & Delete
+module.exports = DbConnection;
